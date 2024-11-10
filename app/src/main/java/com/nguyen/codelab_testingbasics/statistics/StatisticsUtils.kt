16a596dd59d07c2f53d446c2e9009de02a4a0871
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2024-11-09 17:12:46 -0800

    b3: Task: Writing more tests

--- app/src/main/java/com/nguyen/codelab_testingbasics/statistics/StatisticsUtils.kt
+++ app/src/main/java/com/nguyen/codelab_testingbasics/statistics/StatisticsUtils.kt
@@ -6,12 +6,14 @@ import com.nguyen.codelab_testingbasics.data.Task
  * Function that does some trivial computation. Used to showcase unit tests.
  */
 internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
-    val totalTasks = tasks!!.size
+    if (tasks.isNullOrEmpty())
+        return StatsResult(0f, 0f)
+
+    val totalTasks = tasks.size
     val numberOfActiveTasks = tasks.count { it.isActive }
-    return StatsResult(
-        activeTasksPercent = 100f * numberOfActiveTasks / tasks.size,
-        completedTasksPercent = 100f * (totalTasks - numberOfActiveTasks) / tasks.size
-    )
+    val active = 100f * numberOfActiveTasks / totalTasks
+    val completed = 100f * (totalTasks - numberOfActiveTasks) / totalTasks
+    return StatsResult(active, completed)
 }
 
 data class StatsResult(val activeTasksPercent: Float, val completedTasksPercent: Float)
--- app/src/test/java/com/nguyen/codelab_testingbasics/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/nguyen/codelab_testingbasics/statistics/StatisticsUtilsTest.kt
@@ -8,15 +8,58 @@ import org.junit.Test
 class StatisticsUtilsTest {
 
     @Test
-    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
-        // Create an active task
-        val tasks = listOf(Task("title", "desc", false))
+    fun getActiveAndCompletedStats_empty_returnsZeros() {
+        val tasks = emptyList<Task>()
 
-        // Call your function
         val result = getActiveAndCompletedStats(tasks)
 
-        // Check the result
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_noActive_returnsZeroHundred() {
+        val tasks = listOf(Task("title", "desc", true))
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(100f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_error_returnsZeros() {
+        val tasks = null
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        val tasks = listOf(Task("title", "desc", false))
+
+        val result = getActiveAndCompletedStats(tasks)
+
         assertThat(result.activeTasksPercent, `is`(100f))
         assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_both_returnsFortySixty() {
+        val tasks = listOf(
+            Task("one", "one", false),
+            Task("two", "two", false),
+            Task("three", "three", true),
+            Task("four", "four", true),
+            Task("five", "five", true),
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertThat(result.activeTasksPercent, `is`(40f))
+        assertThat(result.completedTasksPercent, `is`(60f))
     }
 }

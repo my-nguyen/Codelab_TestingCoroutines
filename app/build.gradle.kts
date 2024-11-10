4f3a5afd6c4734fd8efa73f2bd7d2afac80e85a6
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2024-11-09 16:43:58 -0800

    b1: Baseline

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.androidx.navigation.safeargs)
+    alias(libs.plugins.jetbrains.kotlin.kapt)
+}
+
+android {
+    namespace = "com.nguyen.codelab_testingbasics"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.nguyen.codelab_testingbasics"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.bundles.room)
+    kapt(libs.room.compiler)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.timber)
+    implementation(libs.coroutines)
+}
---
+++ app/src/main/java/com/nguyen/codelab_testingbasics/statistics/StatisticsUtils.kt
@@ -0,0 +1,17 @@
+package com.nguyen.codelab_testingbasics.statistics
+
+import com.nguyen.codelab_testingbasics.data.Task
+
+/**
+ * Function that does some trivial computation. Used to showcase unit tests.
+ */
+internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
+    val totalTasks = tasks!!.size
+    val numberOfActiveTasks = tasks.count { it.isActive }
+    return StatsResult(
+        activeTasksPercent = 100f * numberOfActiveTasks / tasks.size,
+        completedTasksPercent = 100f * (totalTasks - numberOfActiveTasks) / tasks.size
+    )
+}
+
+data class StatsResult(val activeTasksPercent: Float, val completedTasksPercent: Float)

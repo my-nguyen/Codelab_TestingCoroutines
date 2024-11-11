652384b1d3ec0b28b485c2daba634fffe68278ab
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2024-11-11 09:02:36 -0800

    b1: Baseline

---
+++ app/src/main/java/com/nguyen/codelab_testingcoroutines/statistics/StatisticsFragment.kt
@@ -0,0 +1,39 @@
+package com.nguyen.codelab_testingcoroutines.statistics
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.nguyen.codelab_testingcoroutines.R
+import com.nguyen.codelab_testingcoroutines.databinding.StatisticsFragBinding
+import com.nguyen.codelab_testingcoroutines.util.setupRefreshLayout
+
+/**
+ * Main UI for the statistics screen.
+ */
+class StatisticsFragment : Fragment() {
+
+    private lateinit var viewDataBinding: StatisticsFragBinding
+
+    private val viewModel by viewModels<StatisticsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding = DataBindingUtil.inflate(
+            inflater, R.layout.statistics_frag, container,
+            false
+        )
+        return viewDataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewDataBinding.viewmodel = viewModel
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+    }
+}
---
+++ app/src/main/java/com/nguyen/codelab_testingcoroutines/statistics/StatisticsUtils.kt
@@ -0,0 +1,19 @@
+package com.nguyen.codelab_testingcoroutines.statistics
+
+import com.nguyen.codelab_testingcoroutines.data.Task
+
+/**
+ * Function that does some trivial computation. Used to showcase unit tests.
+ */
+internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
+    if (tasks.isNullOrEmpty())
+        return StatsResult(0f, 0f)
+
+    val totalTasks = tasks.size
+    val numberOfActiveTasks = tasks.count { it.isActive }
+    val active = 100f * numberOfActiveTasks / totalTasks
+    val completed = 100f * (totalTasks - numberOfActiveTasks) / totalTasks
+    return StatsResult(active, completed)
+}
+
+data class StatsResult(val activeTasksPercent: Float, val completedTasksPercent: Float)
